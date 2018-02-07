6de4bd3ab595213603f552ba112b95fff039f8f6
dslosky-usgs <dslosky@usgs.gov>
2018-02-07 03:34:13 -0700

    Init commit with map

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'shakemap-view-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    // Create map (center on US)
+    let map = L.map('map', {
+      center: [ 38, -95 ],
+      zoom: 4,
+      scrollWheelZoom: false
+    });
+
+    // Add street map tile layer
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + 'pk.eyJ1IjoiZHNsb3NreSIsImEiOiJjaXR1aHJnY3EwMDFoMnRxZWVtcm9laWJmIn0.1C3GE0kHPGOpbVV9kTxBlQ', {
+            maxZoom: 18,
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                'Imagery � <a href="http://mapbox.com">Mapbox</a>',
+            id: 'mapbox.streets'
+        }).addTo(map);
+  }
+
+}
