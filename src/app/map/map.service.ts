a030d5936e15e60a1ef9df963b5addb116e6ddc2
dslosky-usgs <dslosky@usgs.gov>
2018-02-08 06:21:30 -0700

    Init docs

--- src/app/map/map.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
-
-@Injectable()
-export class MapService {
-  public plotEvent = new ReplaySubject(1);
-  constructor() { }
-
-}
