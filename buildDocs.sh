76a1398ab108ffd0758a943753edf38c5de18f19
dslosky-usgs <dslosky@usgs.gov>
2018-02-09 06:03:36 -0700

    Updated doc building script

--- buildDocs.sh
+++ buildDocs.sh
@@ -1,6 +1,13 @@
-ng build --base-href "https://dslosky-usgs.github.io/angular-shakemap-view/"
 rm -rf docs/htdocs/*
+ng build --base-href "https://dslosky-usgs.github.io/angular-shakemap-view/" --output-path=docs/dist
+cp -r docs/dist/* docs/htdocs/
+rm -rf docs/dist
+
 cp docs/products.json docs/htdocs/
 cp docs/configs.json docs/htdocs/
 cp -r docs/shakemaps docs/htdocs/
-cp -r dist/* docs/htdocs/
+
+cd docs/htdocs
+git add .
+git commit -m "Auto Update"
+git push origin gh-pages
