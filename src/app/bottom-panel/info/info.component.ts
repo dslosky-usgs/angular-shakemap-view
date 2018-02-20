dca1bd685a9440ee1982bb3ba7c4e1c53df33361
dslosky-usgs <dslosky@usgs.gov>
2018-02-20 08:47:36 -0700

    Info component working

--- src/app/bottom-panel/info/info.component.ts
+++ src/app/bottom-panel/info/info.component.ts
@@ -13,19 +13,21 @@ export class InfoComponent implements OnInit, OnDestroy {
   public objKeys: any = Object.keys;
   
   public name_order = {
-    'event_information': ['event_description', 'event_id', 'magnitude', 'depth', 'longitude', 'latitude', 'origin_time', 'src_mech', 'location', 'fault_ref', 'seismic_stations', 'dyfi'],
-    'ground_motions': ['intensity', 'pga', 'pgv', 'SA(0.3)', 'SA(1.0)', 'SA(3.0)', 'bias'],
+    'event_information': ['event_description', 'event_id', 'magnitude', 'depth', 'longitude', 'latitude', 'origin_time', 'src_mech', 'location', 'fault_ref', 'seismic_stations', 'intensity_observations'],
+    'ground_motions': ['intensity', 'mmi', 'MMI', 'pga', 'PGA', 'pgv', 'PGV', 'SA(0.3)', 'psa03', 'SA(1.0)', 'psa10', 'SA(3.0)', 'psa30', 'bias'],
     'map_information': ['grid_span', 'grid_spacing', 'grid_points', 'min', 'max'],
     'uncertainty': ['mean_uncertainty_ratio', 'grade', 'total_flagged_mi', 'total_flagged_pgm'],
-    'ground_motion_modules': ['gmpe', 'ipe', 'gmice', 'igmice', 'ccf', 'directivity', 'basin'],
-    'shakemap_versions': ['shakemap_revision_id', 'map_version', 'process_time'],
-    'miscellaneous': ['bias_max_mag', 'bias_max_range', 'bias_log_amp', 'bias_max_bias', 'bias_min_bias', 'bias_min_stations', 'bias_norm', 'median_dist', 'outlier_deviation_level', 'outlier_max_mag']
+    'ground_motion_modules': ['gmpe', 'ipe', 'gmice', 'igmice', 'ccf', 'directivity', 'basin_correction'],
+    'shakemap_versions': ['shakemap_revision', 'shakemap_revision_id', 'map_version', 'process_time'],
+    'miscellaneous': ['bias_max_mag', 'bias_max_range', 'bias_log_amp', 'bias_max_bias', 'bias_min_bias', 'bias_min_stations', 'bias_norm', 'median_dist', 'outlier_deviation_level', 'outlier_max_mag'],
+    'roi': ['gm', 'intensity'],
+    'site_response': ['vs30default', 'site_correction']
   }
 
   public names: any = {
     'event_information': {
       'location': 'Location',
-      'longitude': 'longitude',
+      'longitude': 'Longitude',
       'seismic_stations': 'Number of Seismic Stations',
       'magnitude': 'Magnitude', 
       'depth': 'Depth',
@@ -34,14 +36,19 @@ export class InfoComponent implements OnInit, OnDestroy {
       'latitude': 'Latitude', 
       'fault_ref': 'Fault References', 
       'src_mech': 'Mechanism Source',
-      'dyfi': "Number of DYFI Stations"
+      'intensity_observations': "Number of DYFI Stations"
     },
     'ground_motions': {
-      'PGA': 'PGA',
-      'PGV': 'PGV',
+      'PGA': 'PGA (%g)',
+      'PGV': 'PGV (cm/s)',
+      'pga': 'PGA (%g)',
+      'pgv': 'PGV (cm/s)',
       'SA(0.3)': 'PSA03',
       'SA(1.0)': 'PSA10',
       'SA(3.0)': 'PSA30',
+      'psa03': 'PSA03',
+      'psa10': 'PSA10',
+      'psa30': 'PSA30',
       'bias': 'Bias',
       'MMI': 'Intensity'
     },
@@ -69,10 +76,11 @@ export class InfoComponent implements OnInit, OnDestroy {
       },
     'shakemap_versions': {
         'map_version': 'Map',
-        'shakemap_revision_id': 'Code',
+        'shakemap_revision_id': 'GitHub commit',
+        'shakemap_revision': 'Code',
         'process_time': 'Date'
       },
-    'micellaneous': {
+    'miscellaneous': {
         'bias_max_mag': 'Max magnitude to compute bias',
         'bias_max_range': 'Maximum distance to include station in bias',
         'bias_log_amp': 'Use log amp to compute bias',
@@ -83,7 +91,15 @@ export class InfoComponent implements OnInit, OnDestroy {
         'median_dist': 'Median distance used',
         'outlier_deviation_level': 'Outlier level (# of std dev)',
         'outlier_max_mag': 'Max magnitude to flag outliers',
-      }
+      },
+    'roi': {
+      'gm': 'Ground Motion',
+      'intensity': 'Intensity'
+    },
+    'site_response': {
+      'site_correction': 'Site correction applied',
+      'vs30default': 'Reference rock Vs30'
+    }
   }
   
   constructor(public infoService: InfoService) { }
